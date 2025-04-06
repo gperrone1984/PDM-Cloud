8fddb864c800e9651e83c59767042fedef8988fa
gperrone1984 <perrone.gabriele1@gmail.com>
2025-04-06 17:08:17 +0200

    Create 3_Coming_Soon.py

---
+++ pdm_utility_hub/pages/3_Coming_Soon.py
@@ -0,0 +1,18 @@
+# pages/3_Coming_Soon.py
+import streamlit as st
+
+st.set_page_config(
+    page_title="Coming Soon",
+    page_icon="🚧"
+)
+
+st.title("🚧 Coming Soon")
+
+# Puoi aggiungere un'immagine più attinente se vuoi
+# st.image("your_image_path_or_url.jpg", caption="Work in progress!", width=300)
+
+st.info("This section is currently under development. New utilities and tools will be added here in the future.")
+st.write("Stay tuned for updates!")
+
+# Effetto opzionale
+st.balloons()
