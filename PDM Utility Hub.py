cc4d5ee7f43a3b50114c547458743f6df4640da5
gperrone1984 <perrone.gabriele1@gmail.com>
2025-04-06 14:58:48 +0200

    Add files via upload

---
+++ PDM Utility Hub.py
@@ -0,0 +1,36 @@
+import streamlit as st
+from bundle_images import bundle_images_app
+from repository_images import repository_images_app
+
+# ------------------------------
+# Page config
+# ------------------------------
+st.set_page_config(
+    page_title="PDM Utility Hub",
+    page_icon="🧰",
+    layout="wide"
+)
+
+# ------------------------------
+# Sidebar
+# ------------------------------
+st.sidebar.title("🧰 PDM Utility Hub")
+st.sidebar.markdown("### Select a tool")
+
+app_choice = st.sidebar.radio(
+    "",
+    ("Bundle & Set Images Creator", "Repository Image Download & Renaming", "Coming Soon")
+)
+
+# ------------------------------
+# Main Area
+# ------------------------------
+if app_choice == "Bundle & Set Images Creator":
+    bundle_images_app()
+
+elif app_choice == "Repository Image Download & Renaming":
+    repository_images_app()
+
+elif app_choice == "Coming Soon":
+    st.title("🚧 Coming Soon")
+    st.write("This section is under development. Stay tuned!")
